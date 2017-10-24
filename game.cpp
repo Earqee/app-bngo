35a459672cea36cea4a04fe87d7424270ed5eedb
Thiago José <32231425+thiagoj0se@users.noreply.github.com>
2017-10-24 17:41:38 -0300

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,25 @@
+#include "libs.h"
+#include "init.h"
+#include "basic_functions.h"
+#include "classes.h"
+#include "player.h"
+#include "bullet.h"
+#include "main_game.h"
+#include "menu.h"
+
+
+
+int main( void )
+{
+	//initializing libs
+	if(!initialize()) return INVALID;
+
+	//allegro variables
+	display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	if(!display) return INVALID;
+
+	menu();
+
+	al_destroy_display(display);
+	return 0;
+}
