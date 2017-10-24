2af777d545a5707e7170e221b858b39205c04bbb
Thiago José <32231425+thiagoj0se@users.noreply.github.com>
2017-10-24 18:30:54 -0300

    Update main_game.h

--- main_game.h
+++ main_game.h
@@ -14,12 +14,14 @@ void main_game()
 	background_music = al_load_sample("audio/Organ 67 - Fall of an Empire - Intense Powerful Organ Music.ogg");
 	al_play_sample(background_music, 1.0, 0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
 
+	//Frame delayed
+	Frame Player_Shot(5);
+
 	while(!game_over)
 	{
 
 		al_get_keyboard_state(&keyboard_state);
 		al_get_mouse_state(&mouse_state);
-
 
 		al_clear_to_color(al_map_rgb(255,255,255));
 
@@ -28,8 +30,14 @@ void main_game()
 		cam_x = player.x - DISPLAY_WIDTH/2;
 		cam_y = player.y - DISPLAY_HEIGHT/2;
 
-		if(mouse_state.buttons & 1)
+		if(Player_Shot.frameCount > 0)
+			Player_Shot.frameCount--;
+
+		if((mouse_state.buttons & 1) && (!Player_Shot.frameCount))
 		{
+			Player_Shot.frameCount = Player_Shot.frameDelay;
+
+			al_play_sample(player.audio[SHOT], 1.0, 0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
 			bullet = new Bullet(bullet_base,player);
 			bullet_vector.push_back(bullet);
 		}
