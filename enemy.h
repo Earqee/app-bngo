a04ad5296a5be0a2ba69db5d3077601fede0fe57
valeriojr <32737312+valeriojr@users.noreply.github.com>
2017-10-25 13:44:49 -0300

    Update enemy.h
    apaguei as caracteristicas dos inimigos da classe base, cada uma vai ter usa propria classe derivada dessa A

--- enemy.h
+++ enemy.h
@@ -4,178 +4,65 @@
 class Enemy : public Walker
 {
 public:
-	int type;
-	int state;
-	int shotTime;
-	int shotDelay;
+	bool vulnerable;
 
-	Enemy(int type, int shotDelay);
-
- 	void move(Player &player);
-	void round(Player &player);
-	GENERIC_FUNCTION void draw(Generic &generic, bool colliding);	
+	Enemy();
+	
+	void damage();
+	int bullet_collision();
 };
 
-Enemy::Enemy(int type, int shotDelay)
+Enemy::Enemy()
 {
-	this->type = type;
-
-	this->shotTime = 0;
-	this->shotDelay = shotDelay;
+	vulnerable = true;
 	
-	isAlive = true;
-	state = MOVE;
-
-	if(rand()%2)
+	switch(rand()%4)
 	{
-		if(rand()%2)
-		{
-			x = rand() % WINDOW_WIDTH;
-			y = - LIMIT;
-		}
-		else
-		{
-			x = rand() % WINDOW_WIDTH; 
-			y = WINDOW_HEIGHT + LIMIT;
-		}		
+		case 0:						//lado superior da tela
+			{
+				x = rand()%DISPLAY_WIDTH;
+				y = 0 - 96;
+			}
+			break;
+		case 1:
+			{
+				x = rand()%DISPLAY_WIDTH;
+				y = DISLPLAY_HEIGHT + 96;
+			}
+			break;
+		case 2:
+			{
+				x = 0 - 96;
+				y = rand()%DISPLAY_HEIGHT;
+			}
+		case 3:
+			{
+				x = DISPLAY_WIDTH + 96;
+				y = rand()%DISPLAY_HEIGHT;
+			}
 	}
-	else
+}
+
+int Enemy::bullet_collision()
+{
+	for(unsigned int i = 0;i < bullet_vector.size();i++)
 	{
-		if(rand()%2)
+		if(x - bullet_vector[i].x < width/2 + bullet_vector[i].width/2 
+		  && y - bullet_vector[i].y < height/2 + bullet_vector[i].height/2)
 		{
-			x = - LIMIT ;
-			y = rand() % WINDOW_HEIGHT;
+			return 1;
 		}
-		else
-		{
-			x =	WINDOW_WIDTH + LIMIT; 
-			y = rand() % WINDOW_HEIGHT;
-		}	
-	}	
+	}
 	
-	switch(type)
-	{
-		case ASSAULT:
-		{
-			image = al_load_bitmap("images/assault.png");
-			shot_audio = al_load_sample("audio/Space Gun 04.wav");
-			shot_image = al_load_bitmap("images/laser3.png");
-			bound_x = 110;
-			bound_y = 110;	
-			life = 100;
-			speed = DEFAULT/1.5;
-			break;
-		}
-		case SCOUT:
-		{
-			image = al_load_bitmap("images/scout.png");
-			shot_audio = al_load_sample("audio/Space Gun 03.wav");
-			shot_image = al_load_bitmap("images/laser2.png");
-			bound_x = 110;
-			bound_y = 110;
-			life = 100;
-			speed = DEFAULT/1.5;
-			break;
-		}
-		case SUPPORT:
-		{
-			image = al_load_bitmap("images/mutant.png");
-			shot_audio = al_load_sample("audio/Space Gun 12.wav");
-			shot_image = al_load_bitmap("images/laser1.png");
-			bound_x = 110;
-			bound_y = 110;
-			life = 150;
-			speed = DEFAULT/1.5;
-			break;
-		}
-		case TECH:
-		{
-			image = al_load_bitmap("images/tech1.png");
-			shot_audio = al_load_sample("audio/Space Gun 12.wav");
-			shot_image = al_load_bitmap("images/laser1.png");
-			bound_x = 124;
-			bound_y = 124;
-			life = 400;
-			speed = DEFAULT/1.75;
-			break;
-		}
-		case CANNONER:
-		{
-			image = al_load_bitmap("images/cannoner.png");
-			shot_audio = al_load_sample("audio/Space Gun 15.wav");
-			shot_image = al_load_bitmap("images/laser4.png");
-			bound_x = 170;
-			bound_y = 170;
-			life = 600;	
-			speed = DEFAULT/2.0;
-			break;
-		}
-	}
+	return 0;
 }
 
-GENERIC_FUNCTION
-void Enemy::move(Generic &generic)
+void Enemy::damage()
 {
-	angle = getAngle(generic.x, generic.y, this->x, this->y);
-
-	director_x = cos(angle);
-	director_y = sin(angle);
-
-	if(!(abs(director_x) && abs(director_y)))
+	if(bullet_collision() && vulnerable = true)
 	{
-		x += speed * director_x ;
-		y += speed * director_y;
-	}
-	else
-	{
-		x += speed * director_x * RAIZ2SOBRE2;
-		y += speed * director_y * RAIZ2SOBRE2;
-	}
-
-	if(abs(x - generic.x) < 250 && abs(y - generic.y) < 250)
-	{
-		state = ROUND;
+		health--;
 	}
 }
-
-
-void Enemy::round(Player &player)
-{
-	angle = getAngle(generic.x, generic.y, this->x, this->y);
-
-	x -= speed * cos(angle);
-	y -= speed * cos(angle);
-
-	if(abs(x - generic.x) < 200 || abs(y - generic.y) < 200)
-		state = MOVE;	
-}
-
-void Enemy::update()
-{
-	if(!colliding)
-	{
-		switch(state)
-		{
-			case MOVE:
-			move(generic);
-			break;
-
-			case ROUND:
-			round(generic);
-			break;
-		}
-	}
-	else
-	{
-		angle = getAngle(generic.x, generic.y, this->x, this->y);
-	}
-}
-
-void Enemy::draw(Generic &generic, bool colliding)
-{
-	al_draw_rotated_bitmap(image, bound_x/2, bound_y/2, this->x, this->y, angle, 0);
-}	
-
-
 
 #endif
