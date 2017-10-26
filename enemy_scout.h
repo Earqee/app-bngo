273be5fb6630a6a922fa6440da63693d94a9d762
Thiago José <tjss@ic.ufal.br>
2017-10-26 19:28:47 -0300

    Update enemy_scout.h

--- enemy_scout.h
+++ enemy_scout.h
@@ -1,54 +1,66 @@
-#ifndef ENEMY_SCOUT
-#define ENEMY_SCOUT
+#ifndef ENEMY_FOLLOWER
+#define ENEMY_FOLLOWER
 
-class Enemy_scout : public Enemy
+class Enemy_follower : public Enemy
 {
 public:
-    Enemy_scout();
-    
+    Enemy_follower();
+
     void move();
     
     void update(Player &player);
     void draw();
 };
 
-Enemy_scout::Enemy_scout()
+
+
+
+Enemy_follower::Enemy_follower()
 {
-    image = al_load_bitmap("images/scout.png");
-    shot_audio = al_load_sample("audio/Space Gun 04.wav");
-    shot_image = al_load_bitmap("images/laser3.png");
-    bound_x = 110;
-    bound_y = 110;	
+    // Animation Settings
+    char nome_sprite[] = "images/enemy0/dog000/0-0.png";
+    animation_quantity = 2;
+    frames_quantity[WALKING_E_SC] = 4;
+    frames_quantity[IDLE_E_SC] = 1;
+    current_animation = WALKING_E_SC;
+    current_frame = 0;
+    
+    for(int i = 0;i < animation_quantity;i++)
+    {
+        for(int j = 0;j < frames_quantity[i];j++)
+        {
+            nome_sprite[21] = i + 48;
+            nome_sprite[23] = j + 48;
+
+            sprites[i][j] = al_load_bitmap(nome_sprite);
+        }
+    }
+    
     life = 100;
     speed = DEFAULT_SPEED/1.5;
 }
 
-void Enemy_scout::update(Player &player)
+void Enemy_follower::update(Player &player)
 {
     direction = getAngle(x,y,player.x,player.y) + PI;
     
     damage();
     
-    double _angle = getAngle(player.x,player.y,x,y);
-    
-    if(_angle < player.angle - deg_to_rad(90)               //se estiver fora do campo de visao do jogador, a direcao continua normal
-    && _angle > player.angle + deg_to_rad(90))              //senao, faz uma trajetoria circular em relacao ao jogador
+    if(!enemy_collision())
     {
-        direction += deg_to_rad(90);                                          
+        move();
     }
-    
-    move();
     
     draw();
 }
 
-void Enemy_scout::draw()
+void Enemy_follower::draw()
 {
-    al_draw_rectangle(this->x - cam_x, this->y - cam_y, x + bound_x - cam_x, y + bound_y - cam_y, al_map_rgb(0, 0, 0), 5);
-    al_draw_rotated_bitmap(image,bound_x/2,bound_y/2,x-cam_x,y-cam_y,direction,0);
+    al_draw_rectangle(this->x - cam_x - bound_x/2, this->y - cam_y - bound_y/2, x + bound_x/2 - cam_x, y + bound_y/2 - cam_y, al_map_rgb(0, 0, 0), 5);
+    al_draw_rotated_bitmap(sprites[current_animation][current_frame],bound_x/2,bound_y/2,x-cam_x,y-cam_y,direction,0);
 }
 
-void Enemy_scout::move()
+void Enemy_follower::move()
 {
     x += speed*cos(direction);
     y += speed*sin(direction);
