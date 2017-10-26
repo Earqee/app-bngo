3fc9ec4c8ad3e922acf2c78a091e8c558c92b7ef
Thiago José <tjss@ic.ufal.br>
2017-10-26 19:28:22 -0300

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -5,6 +5,7 @@ class Enemy : public Walker
 {
 public:
 	bool vulnerable;
+	int state;
 	ALLEGRO_SAMPLE* shot_audio;
 
 	Enemy();
@@ -14,6 +15,7 @@ public:
 	void damage();
 	int bullet_collision();
 	int enemy_collision();
+	int player_collision(Player &player);
 };
 
 //Enemies vector
@@ -22,6 +24,8 @@ std::vector <Enemy*> Enemies;
 Enemy::Enemy()
 {
 	vulnerable = true;
+	bound_x = 64;
+	bound_y = 64;
 	
 	switch(rand()%4)
 	{
@@ -54,8 +58,8 @@ int Enemy::bullet_collision()
 {
 	for(unsigned int i = 0;i < bullet_vector.size();i++)
 	{
-		if(x - bullet_vector[i]->x < bound_x/2 + bullet_vector[i]->bound_x/2 
-		  && y - bullet_vector[i]->y < bound_y/2 + bullet_vector[i]->bound_y/2)
+		if(abs(x - bullet_vector[i]->x) < bound_x/2 + bullet_vector[i]->bound_x/2 
+		  && abs(y - bullet_vector[i]->y) < bound_y/2 + bullet_vector[i]->bound_y/2)
 		{
 			delete bullet_vector[i];
 			bullet_vector.erase(bullet_vector.begin()+i);
@@ -69,9 +73,11 @@ int Enemy::bullet_collision()
 
 int Enemy::enemy_collision()
 {
-	double x_linha = x + (bound_x/2)*speed*cos(direction);
-	double y_linha = y + (bound_y/2)*speed*sin(direction);
+	double x_linha = x + speed*cos(direction);
+	double y_linha = y + speed*sin(direction);
 	
+	al_draw_rectangle(x_linha - cam_x - bound_x/2, y_linha - cam_y - bound_y/2, x_linha + bound_x/2 - cam_x, y_linha + bound_y/2 - cam_y, al_map_rgb(0, 0, 0), 5);
+
 	for(unsigned int i = 0;i < Enemies.size();i++)
 	{
 		if(x != Enemies[i]->x && y != Enemies[i]->y)
@@ -82,6 +88,19 @@ int Enemy::enemy_collision()
 			}
 		}		
 	}
+	return 0;
+}
+
+int Enemy::player_collision(Player &player)
+{
+	double x_linha = x;
+	double y_linha = y;
+	
+	if(abs(x_linha - player.x) < bound_x/2 + player.bound_x/2 && abs(y_linha - player.y) < bound_y/2 + player.bound_y/2)
+	{
+		return 1;
+	}			
+	
 	return 0;
 }
 
