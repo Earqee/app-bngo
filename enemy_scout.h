a6185182adc5b75ac748e017784521e9d6b2d101
Thiago José <tjss@ic.ufal.br>
2017-10-26 19:30:30 -0300

    Update enemy_scout.h

--- enemy_scout.h
+++ enemy_scout.h
@@ -1,27 +1,29 @@
-#ifndef ENEMY_FOLLOWER
-#define ENEMY_FOLLOWER
+#ifndef ENEMY_SCOUT
+#define ENEMY_SCOUT
 
-class Enemy_follower : public Enemy
+class Enemy_scout : public Enemy
 {
 public:
-    Enemy_follower();
-
+    Enemy_scout();
+    
     void move();
+    void walking(Player &player);
+    void attacking(Player &player);
     
     void update(Player &player);
     void draw();
 };
 
-
-
-
-Enemy_follower::Enemy_follower()
+Enemy_scout::Enemy_scout()
 {
+    state = WALKING_E_SC;
+
     // Animation Settings
-    char nome_sprite[] = "images/enemy0/dog000/0-0.png";
+    char nome_sprite[] = "images/enemy0/fatso0/0-0.png";
     animation_quantity = 2;
     frames_quantity[WALKING_E_SC] = 4;
     frames_quantity[IDLE_E_SC] = 1;
+    frames_quantity[ATTACK_E_SC] = 2;
     current_animation = WALKING_E_SC;
     current_frame = 0;
     
@@ -35,32 +37,80 @@ Enemy_follower::Enemy_follower()
             sprites[i][j] = al_load_bitmap(nome_sprite);
         }
     }
-    
+
     life = 100;
-    speed = DEFAULT_SPEED/1.5;
+    speed = DEFAULT_SPEED/3;
 }
 
-void Enemy_follower::update(Player &player)
+void Enemy_scout::update(Player &player)
 {
+    switch (state)
+    {
+        case WALKING_E_SC:
+        {
+            walking(player);
+        }
+        break;
+        case ATTACK_E_SC:
+        {
+            attacking(player);
+        }
+        break;
+    }
+}
+
+void Enemy_scout::walking(Player &player)
+{
+	speed = DEFAULT_SPEED/3;
+
     direction = getAngle(x,y,player.x,player.y) + PI;
     
     damage();
     
-    if(!enemy_collision())
+    double _angle = getAngle(player.x,player.y,x,y);
+    
+    if(!(_angle < player.angle + deg_to_rad(90)               //se estiver fora do campo de visao do jogador, a direcao continua normal
+    && _angle > player.angle - deg_to_rad(90)))              //senao, faz uma trajetoria circular em relacao ao jogador
     {
-        move();
+        speed = DEFAULT_SPEED;
+        direction += deg_to_rad(90);
     }
     
+    if(!enemy_collision())
+        move();
+    
+    animate();
     draw();
+
+    if(player_collision(player))
+    {
+        state = ATTACK_E_SC;
+        // Animation Settings
+        current_animation = ATTACK_E_SC;
+        current_frame = 0;
+    }
+
 }
 
-void Enemy_follower::draw()
+void Enemy_scout::attacking(Player &player)
+{
+    damage();
+    animate();
+    if(!player_collision(player))
+    {
+        state = WALKING_E_SC;
+        current_animation = WALKING_E_SC;
+        current_frame = 0;
+    }
+}
+
+void Enemy_scout::draw()
 {
     al_draw_rectangle(this->x - cam_x - bound_x/2, this->y - cam_y - bound_y/2, x + bound_x/2 - cam_x, y + bound_y/2 - cam_y, al_map_rgb(0, 0, 0), 5);
     al_draw_rotated_bitmap(sprites[current_animation][current_frame],bound_x/2,bound_y/2,x-cam_x,y-cam_y,direction,0);
 }
 
-void Enemy_follower::move()
+void Enemy_scout::move()
 {
     x += speed*cos(direction);
     y += speed*sin(direction);
