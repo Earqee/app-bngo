972f600bfa9edc52f71d03d66e870fe6ec7316ee
Thiago José <tjss@ic.ufal.br>
2017-10-26 19:31:56 -0300

    Update libs.h

--- libs.h
+++ libs.h
@@ -13,8 +13,9 @@
 #include <math.h>
 #include <iostream>
 
-#define DISPLAY_WIDTH 720
-#define DISPLAY_HEIGHT 480
+#define DISPLAY_WIDTH 1280
+#define DISPLAY_HEIGHT 720
+#define GROUND_TILE_SIZE 64
 #define FPS 60
 
 #define PI 3.141592
@@ -23,10 +24,18 @@
 #define INVALID -1
 #define MAX_STATES 5
 #define MAX_ANIM 9
+#define MAX_FRAMES 9
 #define MAX_SOUNDS 1
-#define MAX_ENEMIES 1
+#define MAX_ENEMIES 10
+
 
 enum WalkerSound{DAMAGE,DEAD,SHOT};
+
+//PLAYER STATES
+enum STATES_P {WALKING_P,IDLE_P};
+
+//ENEMY SCOUT STATES
+enum STATES_E_SC {WALKING_E_SC, ATTACK_E_SC, IDLE_E_SC};
 
 
 
